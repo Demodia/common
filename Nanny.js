7e212681dad1a1b5be3d08fad980fffb9dfaa175
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-03 22:14:38 +0100

    Update Nanny.js

--- Nanny.js
+++ Nanny.js
@@ -33,7 +33,7 @@ export default function Nanny(State, Path = window.location.pathname, Routes = S
     (path === '/' ? ['/'] : path.split('/').filter(char => char !== '')).reduce(
       (obj, path) => {
         const param = obj.routes.find(r => r.path[0] === ':');
-        return obj.routes.find(r => r.path === path) ? { ...obj.routes.find(r => r.path === path), params: obj.params } : { ...param, params: { ...obj.params, [param.path.slice(1)]: path } };
+        return obj.routes.find(r => r.path === path) ? { ...obj.routes.find(r => r.path === path), params: obj.params } : param ? { ...param, params: { ...obj.params, [param.path.slice(1)]: path } } : { title: 'Not Found', view: () => '404 - Not Found' };
       },
       { routes: Routes }
     );
