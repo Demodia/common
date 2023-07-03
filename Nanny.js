7d6a1d73cc42357af5f0a41ed83164ec34e04673
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-03 22:00:19 +0100

    Update Nanny.js

--- Nanny.js
+++ Nanny.js
@@ -29,6 +29,14 @@ export default function Nanny(State, Path = window.location.pathname, Routes = S
   State.Calculate = (calc, list) => {
     if (!Calcs.some(c => c[0].toString() === calc.toString() && c[1] === list)) Calcs.push([calc, list]);
   };
+  const findRoute = path =>
+    (path === '/' ? ['/'] : path.split('/').filter(char => char !== '')).reduce(
+      (obj, path) => {
+        const param = obj.routes.find(r => r.path[0] === ':');
+        return obj.routes.find(r => r.path === path) ? { ...obj.routes.find(r => r.path === path), params: obj.params } : { ...param, params: { ...obj.params, [param.path.slice(1)]: path } };
+      },
+      { routes: Routes }
+    );
   State.Update = (...transformers) => {
     if (State.Before) setState(State.Before);
 
@@ -69,16 +77,6 @@ export default function Nanny(State, Path = window.location.pathname, Routes = S
       Calcs.filter(calc => !calc[1] || calc[1].split(',').some(prop => newState.hasOwnProperty(prop))).forEach(calc => update(state, calc[0](state)));
       return state;
     }, State);
-  }
-
-  function findRoute(path) {
-    (path === '/' ? ['/'] : path.split('/').filter(char => char !== '')).reduce(
-      (obj, path) => {
-        const param = obj.routes.find(r => r.path[0] === ':');
-        return obj.routes.find(r => r.path === path) ? { ...obj.routes.find(r => r.path === path), params: obj.params } : { ...param, params: { ...obj.params, [param.path.slice(1)]: path } };
-      },
-      { routes: Routes }
-    );
   }
 
   function Render() {
